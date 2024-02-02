eee51576ce77c02c4c5fb06e48ff60fd18377b72
Maxim Alpatski <MaximAlpatski@gmail.com>
2024-02-03 00:28:02 +0400

    Init

---
+++ app/src/main/java/com/example/effectivemobiletester/EffectiveMobileTesterApp.kt
@@ -0,0 +1,7 @@
+package com.example.effectivemobiletester
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EffectiveMobileTesterApp: Application() {}
---
+++ app/src/main/java/com/example/effectivemobiletester/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.effectivemobiletester.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.effectivemobiletester.ui.composable.LoginScreen
+import com.example.effectivemobiletester.ui.theme.EffectiveMobileTesterTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EffectiveMobileTesterTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
